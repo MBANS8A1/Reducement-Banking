3cfe53890ec9fa331182ddfad4f791ea1ac5d751
MBANS8A1 <seanmbng@aol.com>
2025-11-21 19:45:38 +0000

    I added the buttons for the banking operations along with the heading and places where the balance and any loan will be displayed. There are operations to open an account, deposit £150, withdraw £50, request a of £5000 loan, pay a loan off or close the account.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,44 @@ const initialState = {
 };
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Reducement Banking Account</h1>
+      <p>Balance: X</p>
+      <p>Loan: X</p>
+
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Open account
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Deposit £150
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Withdraw £50
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Request a loan of £5000
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Pay loan
+        </button>
+      </p>
+      <p>
+        <button onClick={() => {}} disabled={false}>
+          Close account
+        </button>
+      </p>
+    </div>
+  );
 }
 
 export default App;
